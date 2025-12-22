9321d5cd25c3966c9c6fa148e9f9953ba0e80065
Trần Nguyễn Khang An <khangan.10hlvc@gmail.com>
2025-12-22 22:35:38 +0700

    Create query_01_sales_l12m.sql

---
+++ sql/query_01_sales_l12m.sql
@@ -0,0 +1,27 @@
+---Q1: Calc Quantity of items, Sales value & Order quantity by each Subcategory in L12M
+
+
+
+
+--- Present data of Month-Year, name of subcategory, sale value & order quantity
+SELECT DISTINCT FORMAT_TIMESTAMP('%b %Y', TIMESTAMP(sale.ModifiedDate)) AS Period,
+      subcat.name AS name,
+      SUM(sale.OrderQty) AS qty_item,
+      SUM(sale.LineTotal) AS sale_value,
+      COUNT(DISTINCT sale.SalesOrderID) AS order_qty
+
+
+FROM `adventureworks2019.Sales.SalesOrderDetail` AS sale
+ 
+LEFT JOIN `adventureworks2019.Production.Product` AS product_defi
+    ON sale.ProductID =product_defi.ProductID
+
+
+LEFT JOIN `adventureworks2019.Production.ProductSubcategory` AS subcat
+    ON CAST(product_defi.ProductSubcategoryID AS INT64) = subcat.ProductSubcategoryID
+   
+---Filter L12M    
+WHERE date(sale.ModifiedDate) >= (SELECT DATE_SUB(DATE(MAX(ModifiedDate)) , INTERVAL 12 MONTH)
+                                               FROM `adventureworks2019.Sales.SalesOrderDetail`)
+GROUP BY 1,2
+ORDER BY 2;
